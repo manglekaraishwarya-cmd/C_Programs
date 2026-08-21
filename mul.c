cc1fa731899d3e2597fcba3fa1b89b0002fc0586
Aishwarya Manglekar <manglekaraishwarya@gmail.com>
2026-08-21 18:53:19 +0530

    Program of multiplication

---
+++ mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+        int n1 ,n2 ,mul;
+        printf("Enter first number :");
+        scanf("%d",&n1);
+
+        printf("Enter second number :");
+        scanf("%d",&n2);
+
+        mul = n1 * n2;
+
+        printf("The mul of %d and %d is:%d\n",n1, n2, mul);
+        return 0;
+}
+
