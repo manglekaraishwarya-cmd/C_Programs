1ee21b16eeebe29f769f28d9618b7e1b7a4945ee
Aishwarya Manglekar <manglekaraishwarya@gmail.com>
2026-08-23 19:45:02 +0530

    Program of Upper case of letters

---
+++ upper_caes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character:");
+    scanf("%c", &ch);
+
+    if(ch >= 'A' && ch <= 'Z')
+
+    {
+        printf("The input is upper case letter:\n");
+
+    }
+    else
+    {
+             printf("The input is upper case letter:\n");
+     }
+
+
+    return 0;
+}
+
