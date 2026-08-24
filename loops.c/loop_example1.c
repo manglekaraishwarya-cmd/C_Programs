c12caf3cea389fad071ee2e492d4d148cbb3c539
Aishwarya Manglekar <manglekaraishwarya@gmail.com>
2026-08-24 11:25:57 +0530

    Program of loop

---
+++ loops.c/loop_example1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+        int p;
+
+        for (p = 1; p <= 10; p++)
+        {
+                printf("Hello\n");
+                printf("Hi\n");
+        }
+
+        return 0;
+}
+
