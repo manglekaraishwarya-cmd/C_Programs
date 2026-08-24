5090f7f5589e90c2a126cd46ce8c61bc962dff29
Aishwarya Manglekar <manglekaraishwarya@gmail.com>
2026-08-24 14:25:12 +0530

    Oranize C programs into folder

--- 02_Arithmetic/sum.c
+++ 02_Arithmetic/sum.c
@@ -4,10 +4,10 @@ int main()
 {
 	int n1 ,n2 ,sum;
 	printf("Enter first number :");
-	scanf("%d\n",&n1);
+	scanf("%d",&n1);
 
 	printf("Enter second number :");
-	scanf("%d\n",&n2);
+	scanf("%d",&n2);
 
 	sum = n1 + n2;
 
