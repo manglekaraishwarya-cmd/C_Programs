0451516bff22975c1a919cab6a89d79da5fce400
Aishwarya Manglekar <manglekaraishwarya@gmail.com>
2026-09-04 14:59:41 +0530

    Area of rectangle

---
+++ 07_Geometry/area_of_rectangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, breadth, area;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter breadth: ");
+    scanf("%f", &breadth);
+
+    area = length * breadth;
+
+    printf("Area of Rectangle = %.2f", area);
+
+    return 0;
+}
